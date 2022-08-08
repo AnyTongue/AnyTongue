9f559e243a7d1bfadaed642c598910341a89d35c
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2022-08-08 09:38:23 +0200

    use mysql as db provider

---
+++ src/AnyTongue.Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AnyTongue.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnyTongue.Persistence;
+
+public class AppDbContext : IdentityDbContext<AppUser, AppRole, int>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
--- src/AnyTongue.Web/Program.cs
+++ src/AnyTongue.Web/Program.cs
@@ -1,7 +1,7 @@
+using AnyTongue.Domain.Models;
+using AnyTongue.Persistence;
 using AnyTongue.Web.Areas.Identity;
-using AnyTongue.Web.Data;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 using Serilog;
@@ -15,14 +15,12 @@ public class Program
         builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
 
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
+        builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
-        builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+        builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<AppUser>>();
         builder.Services.AddMudServices();
 
         var app = builder.Build();
