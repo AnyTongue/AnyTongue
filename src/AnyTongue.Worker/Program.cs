e73f21c697a3fa3631e5d8e2e4cb9b38d9a74914
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2022-08-08 03:24:07 +0200

    add worker for decoupled operations

---
+++ src/AnyTongue.Worker/Program.cs
@@ -0,0 +1,31 @@
+using Coravel;
+
+namespace AnyTongue.Worker;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(ConfigureApp)
+            .ConfigureServices(ConfigureServices)
+            .Build();
+
+        host.Services.UseScheduler(scheduler =>
+        {
+
+        });
+
+        host.Run();
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHostedService<Worker>();
+    }
+
+    private static void ConfigureApp(HostBuilderContext context, IConfigurationBuilder configBuilder)
+    {
+
+    }
+}
