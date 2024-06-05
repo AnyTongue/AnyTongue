fb77383ca122eb00eda1c1e8b32b2ef5854739cb
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2024-06-05 23:58:01 +0200

    add CreateCourseFormModel

---
+++ src/AnyTongue.Web.Client/Members/Pages/Upload/CreateCourseFormModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AnyTongue.Web.Client.Members.Pages.Upload;
+
+public class CreateCourseFormModel
+{
+    public required string SourceLanguage { get; set; }
+    public required string TargetLanguage { get; set; }
+}
